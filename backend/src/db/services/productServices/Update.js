2aeed3185e77840e1f11229b1bfe9b7666ea2173
Mark Dacurawat <markmarkifffff@gmail.com>
2020-12-09 12:09:46 -1200

    Complete Update.js

--- backend/src/db/services/productServices/Update.js
+++ backend/src/db/services/productServices/Update.js
@@ -1,16 +1,27 @@
 const Connection = require("../../database.js");
 
-module.exports = async (id, productName, productDescription) => {
+module.exports = async (
+  id,
+  productName,
+  productDescription,
+  productRatings
+) => {
   try {
     const query =
       `UPDATE ` +
       `products ` +
       `SET ` +
       `productName = ?,` +
-      `productDescription = ? ` +
+      `productDescription = ?,` +
+      `productRatings = ? ` +
       `WHERE ` +
       `id = ?`;
-    await Connection(query, [productName, productDescription, id]);
+    await Connection(query, [
+      productName,
+      productDescription,
+      productRatings,
+      id,
+    ]);
 
     return true;
   } catch (e) {
